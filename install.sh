93a61fad1b01c4d046f269a83e4e8ee8d73f8c94
Théo Posty <theoposty@gmail.com>
2020-08-25 09:35:03 +0200

    fix ln

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -e
 	sudo apt-get install mdp;
 	sudo mkdir /var/docs;
 	sudo cp ./docs/* /var/docs/;
-	sudo ls -s ./aide /usr/bin/aide;
+	sudo ln -s ./aide /usr/bin/aide;
 
 }||{
 	echo "Une erreur est survenue !"
