0b15170dc02d9e2e01382c71c306a13ad5dee1d5
Théo Posty <theoposty@gmail.com>
2020-08-25 09:25:49 +0200

    cp -> lien symbolique

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -e
 	sudo apt-get install mdp;
 	sudo mkdir /var/docs;
 	sudo cp ./docs/* /var/docs/;
-	sudo cp ./aide /usr/bin/aide;
+	sudo ls -s ./aide /usr/bin/aide;
 
 }||{
 	echo "Une erreur est survenue !"
